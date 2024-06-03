d7a0eea195bf80b66ec79fd9486dc803a4213640
formateur-glodiee-tshimini <contact@tshimini.fr>
2024-06-03 10:32:34 +0200

    feat: demo POO

---
+++ demo/1-new-ES6/class.js
@@ -0,0 +1,10 @@
+class Person {
+  constructor (name, age) {
+    this.name = name
+    this.age = age
+  }
+
+  show () {
+    console.log('informations', this.name, this.age)
+  }
+}
---
+++ demo/1-new-ES6/objet-litteral.js
@@ -0,0 +1,10 @@
+const glodie = {
+  name: 'Glodie',
+  age: 32,
+  // () => {} ie function() {} arrow function contexte globale avec this, et function() contexte avec l'objet en cours
+  show: () => {
+    console.log('informations', this.name, this.age)
+  }
+
+}
+
