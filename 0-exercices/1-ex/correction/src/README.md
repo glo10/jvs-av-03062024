ee1771bca2a84010c8ed3d4ce0c5f1e24b10f43d
formateur-glodiee-tshimini <contact@tshimini.fr>
2024-06-03 14:39:17 +0200

    feat: correction exercise 1

---
+++ 0-exercices/1-ex/correction/src/README.md
@@ -0,0 +1,7 @@
+# Arborescence des dossiers
+
+- [css/](./css/) : contient les fichiers CSS de l'exercice
+- [data/](./data/) : les villes et les pays au format JSON et XML qui seront abordées plus tard
+- [html/](./html/) : pages HTML
+- [html/_partials/](./html/_partials/) : contenus partiels et isolés de bout de pages HTML qui devront être intégrés dynamique plus tard dans le cours
+- **[js/](./js/) : dossier dans lequel, vous devez travailler pour écrire vos scripts**
---
+++ 0-exercices/1-ex/correction/src/js/index.js
@@ -0,0 +1,96 @@
+class SignIn {
+  /**
+   * Constructeur appelé lors de la création d'un objet à l'aide de l'opérateur new SignIn
+   * @returns void
+   */
+  constructor () {
+    this.inputs = [ // Nos inputs elements dans le DOM + message d'aide
+      {
+        selector: 'input[type=email]',
+        msg: 'Veuillez saisir votre adresse e-mail !'
+      },
+      {
+        selector: 'input[type=password]',
+        msg: 'Veuillez saisir votre mot de passe !'
+      }
+    ]
+    this.submit = 'input[type=submit]'
+  }
+
+  /**
+   * Ajout des écouteurs des événements
+   * @returns void
+   */
+  events () {
+    this.onFocusShowHelpMessage()
+    this.onBlurRemoveHelpMessage()
+    this.onSubmitCheckEmptyFields()
+  }
+
+  /**
+   * Gestion de l'événement focus
+   * @returns void
+   */
+  onFocusShowHelpMessage () {
+    const size = this.inputs.length
+    for (let i = 0; i < size; i++) {
+      const inputEl = document.querySelector(this.inputs[i].selector)
+      inputEl.addEventListener('focus', (e) => {
+        e.target.parentElement.firstElementChild.innerHTML = this.inputs[i].msg
+      })
+    }
+  }
+
+  /**
+   * Gestion de l'événement perte de focus (blur)
+   * @returns void
+   */
+  onBlurRemoveHelpMessage () {
+    const size = this.inputs.length
+    for (let i = 0; i < size; i++) {
+      const inputEl = document.querySelector(this.inputs[i].selector)
+      inputEl.addEventListener('blur', (e) => {
+        e.target.parentElement.firstElementChild.innerHTML = ''
+      })
+    }
+  }
+
+  /**
+   * Gestion de l'événement submit
+   * @returns void
+   */
+  onSubmitCheckEmptyFields () {
+    const el = document.querySelector(this.submit)
+    const elts = this.inputs
+    el.addEventListener('click', function (e) {
+      const size = elts.length
+      e.preventDefault()
+      for (let i = 0; i < size; i++) {
+        var alertUser = el.parentElement.querySelector('.alert.alert-danger')
+        var isEmpty = false
+        const inputEl = document.querySelector(elts[i].selector)
+        if (inputEl.value.length === 0) {
+          isEmpty = true
+          if (alertUser === null) {
+            const p = document.createElement('p')
+            p.classList.add('alert', 'alert-danger', 'my-3', 'text-center')
+            p.textContent = elts[i].msg
+            el.insertAdjacentHTML('beforebegin', p.outerHTML)
+          } else {
+            el.parentElement.querySelector('p.alert-danger').innerHTML = elts[i].msg
+          }
+          break
+        }
+      }
+      if (!isEmpty && alertUser) {
+        alertUser.remove()
+      }
+    })
+  }
+}
+// --- EXECUTION
+// Lors du chargement de la page
+window.onload = () => {
+  const signIn = new SignIn()
+  signIn.events()
+}
